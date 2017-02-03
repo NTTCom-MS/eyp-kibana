33454b8923790c8617804c84fda8a73b40c7eaf7
Jordi Prats <jprats@systemadmin.es>
2017-02-03 15:06:10 +0100

    afegit ubnutu 14 i 16

--- manifests/params.pp
+++ manifests/params.pp
@@ -24,6 +24,29 @@ class kibana::params {
         default: { fail('Unsupported RHEL/CentOS version!')  }
       }
     }
+    'Debian':
+    {
+      case $::operatingsystem
+      {
+        'Ubuntu':
+        {
+          case $::operatingsystemrelease
+          {
+            /^14.*$/:
+            {
+              $systemd=false
+            }
+            /^14.*$/:
+            {
+              $systemd=true
+            }
+            default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
+          }
+        }
+        'Debian': { fail('Unsupported')  }
+        default: { fail('Unsupported Debian flavour!')  }
+      }
+    }
     default  : { fail('Unsupported OS!') }
   }
 }
