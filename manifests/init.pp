952155d57b5bade0d8eaf2e687d4b8e2b69711ef
Jordi Prats <jordi.prats@gmail.com>
2018-07-16 13:32:11 +0200

    variablez

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,14 @@
 #
 class kibana(
-              $version           = '5.2.0',
-              $srcdir            = '/usr/local/src',
-              $basedir           = '/opt',
-              $productname       = 'kibana',
-              $manage_service    = true,
-              $host              = '0.0.0.0',
-              $port              = '5601',
-              $elasticsearch_url = 'http://localhost:9200',
+              $manage_package        = true,
+              $package_ensure        = 'installed',
+              $manage_service        = true,
+              $manage_docker_service = true,
+              $service_ensure        = 'running',
+              $service_enable        = true,
+              $host                  = '0.0.0.0',
+              $port                  = '5601',
+              $elasticsearch_url     = 'http://localhost:9200',
             ) inherits kibana::params {
   class { '::kibana::install': }
   # -> class { '::kibana::config': }
