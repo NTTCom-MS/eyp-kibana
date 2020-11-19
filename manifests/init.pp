1134919cf3250daf97a04a0915d8e043817b2031
Marc Herrera <marc.herrera@gmail.com>
2020-11-19 11:32:42 +0100

    typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,12 +8,12 @@ class kibana(
               $host                      = '0.0.0.0',
               $port                      = '5601',
               $elasticsearch_url         = 'http://localhost:9200',
-              $xpack_ml_enabled          = 'undef',
-              $xpack_canvas_enabled      = 'undef', 
-              $timelion_enabled          = 'undef',
-              $xpack_infra_enabled       = 'undef',
-              $xpack_apm_enabled         = 'undef',
-              $xpack_monitoring_enabled  = 'undef',
+              $xpack_ml_enabled          = undef,
+              $xpack_canvas_enabled      = undef, 
+              $timelion_enabled          = undef,
+              $xpack_infra_enabled       = undef,
+              $xpack_apm_enabled         = undef,
+              $xpack_monitoring_enabled  = undef,
             ) inherits kibana::params {
 
   class { '::kibana::install': }
